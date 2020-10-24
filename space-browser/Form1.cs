caf2f700c6e3601b9b30dc448ab9fea5cefe52f1
Kamil Tulczyjew <tulczyjew.kamil@gmail.com>
2020-10-24 18:04:31 +0200

    initial commit

---
+++ space-browser/Form1.cs
@@ -0,0 +1,23 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace space_browser
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
+    
